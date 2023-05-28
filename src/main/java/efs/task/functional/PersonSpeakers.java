d4776f4b73663f81b6f5174b63f93f2dfc8908e0
Zubbek <michal.zub@outlook.com>
2023-05-28 13:58:28 +0200

    Version1

--- src/main/java/efs/task/functional/PersonSpeakers.java
+++ src/main/java/efs/task/functional/PersonSpeakers.java
@@ -19,7 +19,7 @@ class PersonSpeakers {
         //      "HELLO. I'M MARIA.", gdzie Maria to imię danej osoby przekazanej w argumencie metody say().
         //  Wywołanie metody say() interfejsu odbywa się w testach metody.
         //  Aby być pewnym, że użyta jest prawidłowa pisownia zaleca się użycie odpowiedniej stałej.
-        return null;
+        return p -> (HELLO + p.getName()).toUpperCase();
     }
 
     Speaker createGreetLocalSpeaker() {
@@ -28,6 +28,20 @@ class PersonSpeakers {
         //       "Hello. I'm Antonina.", gdzie Antonina to imię danej osoby przekazanej w argumencie metody say().
         //  Wywołanie metody say() interfejsu odbywa się w testach metody.
         //  Aby być pewnym, że użyta jest prawidłowa pisownia zaleca się użycie odpowiednich stałych.
-        return null;
+        return p -> {
+            String greeteings;
+            if(p.getCountryOfLiving() == Country.PL) {
+                greeteings = CZESC + p.getName();
+            } else if (p.getCountryOfLiving()== Country.DE) {
+                greeteings = HALLO + p.getName();
+            }
+            else if (p.getCountryOfLiving() == Country.USA) {
+                greeteings = HELLO + p.getName();
+            }
+            else {
+                throw new IllegalArgumentException("Unsupported language!");
+            }
+            return greeteings;
+        };
     }
 }
